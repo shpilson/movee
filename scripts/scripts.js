8f82a151e66696d9961164d0ca5e8dbdc0137999
shpilson <kovalenko.jora@gmail.com>
2021-08-29 18:11:39 +0300

    update

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,12 @@
+const pages = {
+    all: document.querySelector('.wrapper')
+}
+
 // Всплывающие окна
 import myModal from './scripts/popup/popup.js'; // Модальное окно (на главной)
 import repeatMainModal from './scripts/popup/popupActivate.js'; //  Активирация окна на главной странице
+
+if (pages.all) {
+    myModal();
+    repeatMainModal();
+}
