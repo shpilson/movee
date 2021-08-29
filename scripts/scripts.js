ca3f89b954bd6be10b050184d6700accbf621830
shpilson <kovalenko.jora@gmail.com>
2021-08-29 18:21:15 +0300

    update

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,11 +1,10 @@
 // Всплывающие окна
-import myModal from './popup/popup.js'; // Модальное окно (на главной)
-import repeatMainModal from './popup/popupActivate.js'; //  Активирация окна на главной странице
+import myModal from 'https://shpilson.github.io/movee/scripts/popup/popup.js'; // Модальное окно (на главной)
+import repeatMainModal from 'https://shpilson.github.io/movee/scripts/popup/popupActivate.js'; //  Активирация окна на главной странице
 
 const pages = {
     all: document.querySelector('.wrapper')
 }
-
 
 if (pages.all) {
     myModal();
