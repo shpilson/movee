556432b5f53d1056c01b66971ba870e9c2ccd9ce
shpilson <kovalenko.jora@gmail.com>
2021-09-07 23:54:44 +0300

    update

--- js/lazyload.js
+++ js/lazyload.js
@@ -4,6 +4,10 @@ const windowHeight = document.documentElement.clientHeight;
 let lazyImagesPositions = [];
 if (lazyImages.length > 0) {
     lazyImages.forEach(img => {
-
+        if(img.dataset.src || img.dataset.srcset) {
+            lazyImagesPositions.push(img.getBoundingClientRect().top + pageYOffset);
+        }
     });
 }
+
+console.log(lazyImagesPositions);
